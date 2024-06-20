c647c5fdcfe09e4b4e06a9fb20a7eb7e1d13d1ec
Matheus Costa <matheuscostakrenn@gmail.com>
2024-06-19 23:07:13 -0300

    ajuste vars

--- backend/meutcc/env_settings.py
+++ backend/meutcc/env_settings.py
@@ -10,12 +10,12 @@ DB_PASSWORD=getenv("DB_PASSWORD", "")
 DB_HOST=getenv("DB_HOST", "")
 DB_PORT=getenv("DB_PORT", "")
 
-AUTH_FRONTEND_URL=getenv("AUTH_FRONTEND_URL", "${FRONTEND_URL}/auth?token={token}&data={data}")
-AUTH_ERROR_FRONTEND_URL=getenv("AUTH_ERROR_FRONTEND_URL", "${FRONTEND_URL}/auth")
+AUTH_FRONTEND_URL=getenv("AUTH_FRONTEND_URL", "{FRONTEND_URL}/auth?token={token}&data={data}").format(FRONTEND_URL=FRONTEND_URL)
+AUTH_ERROR_FRONTEND_URL=getenv("AUTH_ERROR_FRONTEND_URL", "{FRONTEND_URL}/auth").format(FRONTEND_URL=FRONTEND_URL)
 
 GOOGLE_OAUTH2_CLIENT_ID=getenv("GOOGLE_OAUTH2_CLIENT_ID", "")
 GOOGLE_OAUTH2_CLIENT_SECRET=getenv("GOOGLE_OAUTH2_CLIENT_SECRET", "")
-GOOGLE_OAUTH2_REDIRECT_URI=getenv("GOOGLE_OAUTH2_REDIRECT_URI", "${APP_URL}/oauth2callback")
+GOOGLE_OAUTH2_REDIRECT_URI=getenv("GOOGLE_OAUTH2_REDIRECT_URI", "{APP_URL}/oauth2callback").format(APP_URL=APP_URL)
 GOOGLE_OAUTH2_SCOPE=['openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
 
 EXTRA_ALLOWED_HOSTS=['127.0.0.1', '.vercel.app']
